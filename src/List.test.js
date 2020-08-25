e2693a2bf55c4d887c09812bc90ec622e1964a7c
Mark Marcello <SwiftTactics@gmail.com>
2020-08-25 14:22:19 -0400

    tests fixed

--- src/List.test.js
+++ src/List.test.js
@@ -2,22 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 import List from './List'
+import Card from './Card'
+import STORE from './STORE'
 
 
 describe('List Component', () =>{
   it('renders without crashing',() => {
     const div = document.createElement('div');
-    ReactDOM.render(<List />, div);
+    ReactDOM.render(<List header={["header"]} cards= {["Card"]} />, div);
     ReactDOM.unmountComponentAtNode(div)
   })
 
   it('renders the UI as expected', () => {
     const tree = renderer.create(<List
       header='header'
-      cards={[
-      {key='key', title='title', content='content'},
-      {key='key2', title='title2', ucontent='content2'}
-    ]}
+      cards={[{id:1, title:'name', content: 'data' }]}
+    
       />).toJSON();
       expect(tree).toMatchSnapshot();
   })
