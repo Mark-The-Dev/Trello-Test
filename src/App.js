54bffbf7ae5c0bbf547af556ca48a53ac509a4d3
Mark Marcello <SwiftTactics@gmail.com>
2020-08-27 12:13:17 -0400

    cleaned up prop deference

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 import List from './List'
+import STORE from './STORE'
 import './App.css';
 
+
 class App extends Component {
-  static defaultProps = {
-    store: {
-      lists: [],
-      allCards: {},
+
+  state = {
+    store: STORE
+  }
+
+  handleAddItem = (itemName) => {
+    const newItems = {
+      ...this.state.store.allCards
     }
-  };
+    this.setState({
+      //shoppingItems: newItems
+    })
+  }
+
+  //  newRandomCard = () => {
+  //   const id = Math.random().toString(36).substring(2, 4)
+  //     + Math.random().toString(36).substring(2, 4);
+  //   return {
+  //     id,
+  //     title: `Random Card ${id}`,
+  //     content: 'lorem ipsum',
+  //   }
+  // }
+
+
+
 
   render() {
     const { store } = this.props
@@ -20,9 +42,7 @@ class App extends Component {
         <div className='App-list'>
           {store.lists.map(list => (
             <List
-              key={list.id}
-              header={list.header}
-              cards={list.cardIds.map(id => store.allCards[id])}
+
             />
           ))}
         </div>
